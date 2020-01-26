7e0643217412adfbc0e40d285d842caa449950f1
kvhq <kvhqll@gmail.com>
2020-01-26 14:46:30 +0300

    [setup, semaphore] Updated shadowjar version and gradle tasks

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -8,7 +8,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.3.61"
-    id("com.github.johnrengelman.shadow") version "2.0.4"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
     // Apply the application plugin to add support for building a CLI application.
     application
 }
