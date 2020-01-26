e0c4693c23c2b2cefbc8987be020d3d07daf29c0
kvhq <kvhqll@gmail.com>
2020-01-26 14:27:06 +0300

    [setup] Added shadowjar

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,14 +3,18 @@
  *
  * This generated file contains a sample Kotlin application project to get you started.
  */
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.3.61"
-
+    id("com.github.johnrengelman.shadow") version "2.0.4"
     // Apply the application plugin to add support for building a CLI application.
     application
 }
+
+group = "xxx.yyy"
+version = "1.0-SNAPSHOT"
 
 repositories {
     // Use jcenter for resolving dependencies.
@@ -30,6 +34,12 @@ dependencies {
 
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
